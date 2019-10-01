42753c0d14186fae0dd3421c8a4b800b968f4177
antocara <antocara@gmail.com>
2019-10-01 21:39:15 +0200

    created dao for renewals data

---
+++ lib/data/database/renewal_dao.dart
@@ -0,0 +1,60 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:subscriptions/data/database/database_provider.dart';
+import 'package:subscriptions/data/entities/renewal.dart';
+import 'package:subscriptions/data/entities/subscription.dart';
+
+class RenewalDao {
+  static const TABLE_NAME = "renewals";
+
+  RenewalDao(Future<Database> database) {
+    this._database = database;
+  }
+
+  Future<Database> _database;
+
+  static final columnId = "id";
+  static final columnSubscriptionId = "subscription_id";
+  static final columnRenewalAt = "renewal_at";
+
+  static String createRenewalTable() {
+    return "CREATE TABLE ${RenewalDao.TABLE_NAME} "
+        "("
+        "$columnId ${DataBaseConstants.INTEGER} ${DataBaseConstants.PRIMARY_KEY}, "
+        "$columnSubscriptionId ${DataBaseConstants.TEXT}, "
+        "$columnRenewalAt ${DataBaseConstants.TEXT}, "
+        "$columnPrice ${DataBaseConstants.REAL}, "
+        "$columnFirstBill ${DataBaseConstants.INTEGER}, "
+        "$columnColor ${DataBaseConstants.INTEGER}, "
+        "$columnRenewal ${DataBaseConstants.INTEGER}, "
+        "$columnRenewalPeriod ${DataBaseConstants.TEXT}"
+        ")";
+  }
+
+  Future<bool> insertRenewal(Renewal renewal) async {
+    final db = await _database;
+
+    final result = await db.insert(
+      TABLE_NAME,
+      renewal.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    return Future.value(result == 1 ? true : false);
+  }
+
+  Future<List<Renewal>> fetchAllRenewalBy({Subscription subscription}) async {
+    final db = await _database;
+
+    String whereString = '$columnSubscriptionId = ?';
+
+    final List<Map<String, dynamic>> maps = await db.query(
+      TABLE_NAME,
+      where: whereString,
+      whereArgs: [subscription.id],
+    );
+
+    return List.generate(maps.length, (i) {
+      return Renewal.fromMap(maps[i]);
+    });
+  }
+}
